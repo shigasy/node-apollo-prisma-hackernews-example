e755150a4467514c63a94fa4d5ad4cd99d0c7b75
shigasy <kxx555u.u@gmail.com>
2021-08-22 01:37:00 +0900

    簡単なクエリ

--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Mutation {
+    post(url: String!, description: String!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+const links = [
+  {
+    id: "ID",
+    description: "desc",
+    url: "url",
+  },
+];
+
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => "info",
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
