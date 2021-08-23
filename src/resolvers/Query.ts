c649acdf8d3247814eeba246436c8431c8dd1e62
shigasy <kxx555u.u@gmail.com>
2021-08-24 05:02:53 +0900

    フィルタリング、ページネーション、Order追加

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,9 +1,34 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Context } from "../@types/context";
+
 const info = () => {
   return "info";
 };
 
-const feed = (parent, args, context, info) => {
-  return context.prisma.link.findMany();
+const feed = async (parent, args, context: Context, info) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : undefined;
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+  const count = await context.prisma.link.count({
+    where,
+  });
+
+  return {
+    links,
+    count,
+  };
 };
 
 export default { info, feed };
